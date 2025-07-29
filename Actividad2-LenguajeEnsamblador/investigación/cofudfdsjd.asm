25f92e06f2a44c8b89306cef53349ac99c8aa983
lejandrwoo <alejomunoz249@gmail.com>
2025-07-29 09:32:57 -0500

    29

---
+++ Actividad2-LenguajeEnsamblador/investigación/cofudfdsjd.asm
@@ -0,0 +1,41 @@
+
+@SCREEN
+D=A
+@R0
+M=D
+
+(LOOP)
+
+    @KBD
+    D=M
+
+
+    @100
+    D=D-A
+
+
+    @DRAW
+    D;JEQ
+
+
+    @ERASE
+    0;JMP
+
+(DRAW)
+   
+    @R0
+    A=M
+    M=-1          
+
+   
+    @LOOP
+    0;JMP
+
+(ERASE)
+ 
+    @R0
+    A=M
+    M=0
+
+    @LOOP
+    0;JMP
